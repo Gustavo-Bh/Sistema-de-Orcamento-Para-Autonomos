458ecde8a0baf29ddb3077aff2c47a642389fa82
openspaceoficial <gustavoalves0706@gmail.com>
2026-02-06 15:51:11 -0300

    Push 2.0 Completo
    Retirada de Vendor e arquivos desnecessários do ambiente de homologação.

--- teste/app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'db_host'   => 'localhost',
-    'db_name'   => 'u566100020_marcioo',
-    'db_user'   => 'u566100020_reet',
-    'db_pass'   => 'Romulo@130948A',
-    'db_charset'=> 'utf8mb4',
-];
